f4e3e5293388c97652d61af4820e19766713bc58
hireahitman <159198420+hireahitman@users.noreply.github.com>
2024-11-03 09:53:04 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,10 +6,3 @@ const hamburger = document.getElementById('hamburger');
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
-
-// Close the menu when clicking outside of it
-window.addEventListener('click', (event) => {
-    if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
-        menu.classList.remove('active');
-    }
-});
